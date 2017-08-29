23c7880bc27fa1acf5b3c6689164f698375c6d95
Jasper <jappe_1999@outlook..com>
2017-08-29 12:54:13 +0200

    Added set_hosts_file method to __main__.py

--- __main__.py
+++ __main__.py
@@ -3,6 +3,12 @@ from hostsfile import HostsFile
 if __name__ == '__main__':
 	domain_list = open('domain-list.txt')
 	hostsf = HostsFile()
+
+	""" If your hostsfile is somewhere different than /etc/hosts,
+	    you can set it with the method below.
+	"""
+	# hostsf.set_hosts_file('')
+
 	for host in map(lambda s: s.strip(), domain_list.readlines()):
 		hostsf.block(host)
 
